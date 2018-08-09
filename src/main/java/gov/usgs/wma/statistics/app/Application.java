4c90d9d00c6ad94c29ac9a4a0bcc2b985e4d084c
David Uselmann <duselmann@usgs.gov>
2018-08-09 11:12:32 -0500

    moved to wma and updated scanned packages

--- src/main/java/gov/usgs/wma/statistics/app/Application.java
+++ src/main/java/gov/usgs/wma/statistics/app/Application.java
@@ -1,10 +1,10 @@
-package gov.usgs.ngwmn.app;
+package gov.usgs.wma.statistics.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(scanBasePackages = "gov.usgs.ngwmn.control")
+@SpringBootApplication(scanBasePackages = "gov.usgs.wma.statistics.control")
 @Configuration
 public class Application {
 
